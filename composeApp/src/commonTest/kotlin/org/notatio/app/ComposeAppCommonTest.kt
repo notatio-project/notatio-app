0f3474d2c7172d2db9e8237699b5521d41525ba1
François SP <francois.sp@ik.me>
2025-06-04 09:26:29 +0200

    chore: add KMP project

---
+++ composeApp/src/commonTest/kotlin/org/notatio/app/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.notatio.app
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
